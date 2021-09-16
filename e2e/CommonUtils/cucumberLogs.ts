b1ad17ed0cf3e776c4912063ea9babc0ae0431ae
Sriram Venkataraman <sriram20mail@gmail.com>
2021-09-16 14:39:05 +0100

    Cucumber logs & package.json

---
+++ e2e/CommonUtils/cucumberLogs.ts
@@ -0,0 +1,26 @@
+import {Status} from 'cucumber';
+import {browser} from 'protractor';
+
+export class CucumberLog
+{
+  public static cucumberLog: any;
+  public static async attachLog(log: any): Promise<void> {
+    CucumberLog.cucumberLog = log;
+  }
+  public static async addScreenshotOnFailure(scenario: any): Promise<void> {
+    if (scenario.result.status === Status.FAILED){
+      await CucumberLog.addScreenshot();
+    }
+  }
+  public static async addJson(obj: any): Promise<void> {
+      CucumberLog.cucumberLog.attach(JSON.stringify(obj), 'application/json');
+  }
+  public static async addText(message: string): Promise<void> {
+    CucumberLog.cucumberLog.attach(message);
+  }
+
+  public static async addScreenshot(): Promise<void> {
+    const screenShotFail = await browser.takeScreenshot();
+    await CucumberLog.cucumberLog.attach(screenShotFail, 'image/png');
+  }
+}
